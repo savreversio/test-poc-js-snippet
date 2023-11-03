7a087dbafd0a916c1871f153e8d18dc4e24ee9c9
Marwen Cherif <m.cherif@revers.io>
2023-11-03 16:00:38 +0100

    Using ShareByLink in JS vanilla project

--- vanilla/src/counter.js
+++
@@ -1,9 +0,0 @@
-let counter = 0;
-const counterElement = document.querySelector('#counter');
-
-function setCounter(num) {
-    counter = num;
-    counterElement.textContent = `${counter}`;
-}
-
-setCounter(counter);
